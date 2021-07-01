557903af26e270fd9e815e80ffd7b3b6e5fff298
Akasha <Akasha.Rojee@gmail.com>
2021-07-01 18:32:47 +0400

    Add HTML and CSS for mobile menu styling

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,9 @@
 const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu-links');
 
 function detect() {
   console.log('menu');
+  menu.classList.toggle('show-menu');
 }
 
 menuButton.addEventListener('click', detect);
