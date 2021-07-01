e6fbdd72d75a273879aef5813112fa90ead76b49
Akasha <Akasha.Rojee@gmail.com>
2021-07-01 21:05:16 +0400

    Check mobile view before operating mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,4 @@
+const mobileView = window.matchMedia('(max-width: 767px)');
 const openButton = document.querySelector('.menu-button');
 const menu = document.querySelector('.menu-links');
 const closeButton = menu.querySelector('button');
@@ -6,12 +7,13 @@ let menuLinks = menu.querySelectorAll('a');
 
 // When mobile menu is opened/closed, set active menu button to close/open button respectively
 function switchMenuButton() {
-  console.log('menu');
-  menu.classList.toggle('show-menu');
-  openButton.classList.toggle('menu-button');
-  closeButton.classList.toggle('menu-button');
-  activeMenuButton = openButton.classList.contains('menu-button') ? openButton : closeButton;
-  activeMenuButton.addEventListener('click', switchMenuButton);
+  if (mobileView.matches) { //operate mobile menu only in mobile view
+    menu.classList.toggle('show-menu');
+    openButton.classList.toggle('menu-button');
+    closeButton.classList.toggle('menu-button');
+    activeMenuButton = openButton.classList.contains('menu-button') ? openButton : closeButton;
+    activeMenuButton.addEventListener('click', switchMenuButton);
+  }
 }
 
 addEventListeners([openButton, ...menuLinks], 'click', switchMenuButton);
