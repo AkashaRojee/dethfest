66c01403f6c4d523874e9894910bf0b93deb3069
Akasha <Akasha.Rojee@gmail.com>
2021-07-02 00:34:43 +0400

    Populate Main Program dynamically

--- js/dynamic-population.js
+++ js/dynamic-population.js
@@ -1,7 +1,12 @@
-
-
-
+let programs = [];
 let bands = [];
+
+//Constructor for each Program
+function Program(image, title, description) {
+  this.image = image;
+  this.title = title;
+  this.description = description;
+}
 
 // Constructor for each Band
 function Band(name, image, country, description) {
@@ -10,6 +15,17 @@ function Band(name, image, country, description) {
   this.country = country;
   this.description = description;
 }
+
+//Create array of Program objects
+programs = [
+  new Program('bands', 'Bands', 'Watch live some of the best Death Metal bands out there on a spectacular stage.'),
+
+  new Program('talks', 'Talks', 'More than growls and blast beats. Hear what musicians have to say.'),
+
+  new Program('shows', 'Shows', 'Adrenaline-fuelled shows with fire acts, acrobatics, sword fights, and more.'),
+
+  new Program('karaoke', 'Karaoke', 'At the end of each day, Metal karaokes to relax around or show your talents.')
+]
 
 // Create array of Band objects
 bands = [
@@ -26,35 +42,61 @@ bands = [
   new Band('Alkaloid', 'alkaloid', 'Germany', 'Defined through the field of tension created between musical opposites, merging extreme metal and other more diverse genres of music into a unique style, making death metal more than just blastbeats and growls.')
 ];
 
-//Create elements for band card
-function createCard(band) {
-  return {
-    'band' : 
-      createElement('div', 'band flex-col m-b-60 d-m-r-45'),
-    'band-image' :
-      createElement('img', 'band-image flex-row m-b-10', 
-      { src: `./images/bands/${band.image}.png`,
-        alt: `Logo of ${band.name}`,
-      }),
-    'band-country' :
-      createElement('span', 'band-country border-bottom flex-col italic color-orange m-b-15', {}, `${band.country}`),
-    'band-description' :
-      createElement('p', 'band-description flex-row font-smallest', {}, `${band.description}`) 
+//Structure elements of program/band card among themselves
+function structureCard(card, object) {
+  switch(object.constructor.name) {
+
+    case 'Program':
+
+      card['program-card'].append(card['program-image'], card['program-title'], card['program-description']);
+      card['program-image'].append(card['image']);
+      return card['program-card'];
+
+    case 'Band':
+
+      card['band'].append(card['band-image'], card['band-country'], card['band-description']);
+      return card['band'];
   }
 }
 
-//Append elements of band card to direct parent
-function structureCard(card) {
-  card['band'].append(card['band-image'], card['band-country'], card['band-description']);
-  return card['band'];
-}
+//Create elements for program/card card
+function createCard(object) {
 
-//Append every 3 band cards to a band col, and append every band col to a band col container
-function appendCard(bandColContainer, bandCol, card, index) {
-  bandCol.append(card);
-  if ((index + 1) % 3 === 0) {
-    bandColContainer.appendChild(bandCol.cloneNode(true));
-    bandCol.innerHTML = '';
+  switch(object.constructor.name) {
+
+    case 'Program':
+
+      return {
+        'program-card' : 
+          createElement('div', 'program-card justify-between d-justify-unset flex-row d-flex-col d-wrap d-w-24 p-20 m-b-15'),
+        'program-image' :
+          createElement('div', 'program-image flex-col d-flex-row justify-center w-30 d-w-100'),
+        'image' :
+          createElement('img', 'h-100 d-w-100 fit-cover', 
+          { src: `./images/program/${object.image}.jpg`,
+            alt: `Photo of ${object.title}`,
+          }),
+        'program-title' :
+          createElement('span', 'program-title flex-col d-flex-row justify-center center-text w-25 d-w-100 color-orange font-big weight-bold vertical-center d-m-t-15', {}, `${object.title}`),
+        'program-description' :
+          createElement('p', 'program-description flex-col d-flex-row justify-center w-40 d-w-100 font-small d-m-t-1', {}, `${object.description}`) 
+      };
+
+    case 'Band':
+
+      return {
+        'band' : 
+          createElement('div', 'band flex-col m-b-60 d-m-r-45'),
+        'band-image' :
+          createElement('img', 'band-image flex-row m-b-10', 
+          { src: `./images/bands/${object.image}.png`,
+            alt: `Logo of ${object.name}`,
+          }),
+        'band-country' :
+          createElement('span', 'band-country border-bottom flex-col italic color-orange m-b-15', {}, `${object.country}`),
+        'band-description' :
+          createElement('p', 'band-description flex-row font-smallest', {}, `${object.description}`) 
+      };
   }
 }
 
@@ -65,11 +107,32 @@ function populateFeaturedSection() {
   let bandCol = createElement('div', 'band-col flex-col w-100 d-w-50');
   bands.forEach((band, index) => {
     let card = createCard(band);
-    card = structureCard(card);
-    appendCard(bandColContainer, bandCol, card, index);
+    card = structureCard(card, band);
+
+    //Append every 3 band cards to a band col, and append every band col to a band col container
+    bandCol.append(card);
+    if ((index + 1) % 3 === 0) {
+      bandColContainer.appendChild(bandCol.cloneNode(true));
+      bandCol.innerHTML = '';
+    }
   });
   bandSection.append(bandColContainer);
 }
 
+//For each program object, create card elements, structure them, and append to parents
+function populateProgramSection() {
+  let programsContainer = document.querySelector('.programs-container');
+  programs.forEach((program, index) => {
+    let card = createCard(program);
+    card = structureCard(card, program);
+    programsContainer.append(card);
+  });
+}
+
+function populateSections() {
+  populateProgramSection();
+  populateFeaturedSection();
+}
+
 // Create HTML for Featured Bands section after whole page has loaded
-window.addEventListener('load', populateFeaturedSection);
+window.addEventListener('load', populateSections);
